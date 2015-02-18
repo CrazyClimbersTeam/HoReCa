e7fd33eaeb836a0e889fd612bca67e4421b0a5f4
unknown <adudka@IT-408.govital.lan>
2015-02-18 16:28:27 +0200

    Jeapie API has been integrated in the app.

--- app/src/main/java/com/crazyclimbersteam/horeca/MainActivity.java
+++ app/src/main/java/com/crazyclimbersteam/horeca/MainActivity.java
@@ -2,18 +2,37 @@ package com.crazyclimbersteam.horeca;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.jeapie.JeapieAPI;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    private final static String TAG = MainActivity.class.getSimpleName();
+
+    private final static String SENDER_ID = "660774978335";
+    private final static int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // init Jeapie service
+        JeapieAPI.init(this);
+
+        // Check device for Play Services APK.
+        if (checkPlayServices()) {
+            // Get GCM token asynchronously in background
+            JeapieAPI.getInstance().registerTokenInBackground(this, SENDER_ID);
+        }
+
         setContentView(R.layout.activity_main);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -35,5 +54,25 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Check the device to make sure it has the Google Play Services APK. If
+     * it doesn't, display a dialog that allows users to download the APK from
+     * the Google Play Store or enable it in the device's system settings.
+     */
+    private boolean checkPlayServices() {
+        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
+                GooglePlayServicesUtil.getErrorDialog(resultCode, this,
+                        PLAY_SERVICES_RESOLUTION_REQUEST).show();
+            } else {
+                Log.i(TAG, "This device is not supported.");
+                finish();
+            }
+            return false;
+        }
+        return true;
     }
 }
