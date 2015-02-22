380d0c39937e883f8c83f08c25fee4c6e8e28440
alexmirash <alex.mirash@gmail.com>
2015-02-22 06:43:22 +0200

    [Categories] default items were update

--- app/src/main/java/com/crazyclimbersteam/horeca/net/pojo/Category.java
+++ app/src/main/java/com/crazyclimbersteam/horeca/net/pojo/Category.java
@@ -7,17 +7,34 @@ package com.crazyclimbersteam.horeca.net.pojo;
  * Time: 14:46
  */
 public class Category {
+    private static final int NO_ID = 0;
 
     private String imageUrl;
     private String name;
+    private int defaultImageId = NO_ID;
 
     public Category(String imageUrl, String name) {
         this.imageUrl = imageUrl;
         this.name = name;
     }
 
+    public Category(String imageUrl, String name, int defaultImageId) {
+        this.imageUrl = imageUrl;
+        this.name = name;
+        this.defaultImageId = defaultImageId;
+    }
+
+    public Category(String name, int defaultImageId) {
+        this("url", name, defaultImageId);
+    }
+
+
     public String getImageUrl() {
         return imageUrl;
+    }
+
+    public int getDefaultImageId() {
+        return defaultImageId;
     }
 
     public void setImageUrl(String imageUrl) {
--- app/src/main/java/com/crazyclimbersteam/horeca/tools/ScreenController.java
+++ app/src/main/java/com/crazyclimbersteam/horeca/tools/ScreenController.java
@@ -33,4 +33,8 @@ public class ScreenController {
             }
         }
     }
+
+    public String getCurrentScreenTag() {
+        return currentScreenTag;
+    }
 }
