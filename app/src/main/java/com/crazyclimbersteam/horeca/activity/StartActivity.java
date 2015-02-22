f22223e82130db4757fc1149257e17dc60900178
damvol <damvol@ukr.net>
2015-02-22 02:09:29 +0200

    refistration logic has been changed

--- app/src/main/java/com/crazyclimbersteam/horeca/activity/StartActivity.java
+++ app/src/main/java/com/crazyclimbersteam/horeca/activity/StartActivity.java
@@ -1,6 +1,8 @@
 package com.crazyclimbersteam.horeca.activity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.app.Fragment;
@@ -12,19 +14,30 @@ import com.crazyclimbersteam.horeca.fragment.welcome.SplashFragment;
 
 public class StartActivity extends ActionBarActivity {
 
-    public static final int DELAY_MILLIS = 1000;
+    public static final int DELAY_MILLIS = 1500;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.start_layout);
+
         getSupportFragmentManager().beginTransaction().add(R.id.main_container, new SplashFragment()).commit();
+
+        final String userName = getSharedPreferences(RegistrationFragment.REGISTRATION_PREFS, Context.MODE_PRIVATE)
+                .getString(RegistrationFragment.USER_NAME, "");
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                getSupportFragmentManager().beginTransaction().
-                        setCustomAnimations(R.anim.enter, R.anim.exit, R.anim.pop_enter, R.anim.pop_exit).
-                            replace(R.id.main_container, new RegistrationFragment(), RegistrationFragment.TAG).commitAllowingStateLoss();
+                if (userName.length() > 0) {
+                    Intent mainActivityIntent = new Intent(StartActivity.this, MainActivity.class);
+                    startActivity(mainActivityIntent);
+                    finish();
+                } else {
+                    getSupportFragmentManager().beginTransaction().setCustomAnimations(R.anim.enter, R.anim.exit, R.anim.pop_enter, R.anim.pop_exit).
+                        replace(R.id.main_container, new RegistrationFragment(),
+                                RegistrationFragment.TAG).commitAllowingStateLoss();
+                }
             }
         }, DELAY_MILLIS);
     }
--- app/src/main/java/com/crazyclimbersteam/horeca/menu/NavigationMenuFragment.java
+++ app/src/main/java/com/crazyclimbersteam/horeca/menu/NavigationMenuFragment.java
@@ -1,13 +1,24 @@
 package com.crazyclimbersteam.horeca.menu;
 
 import android.app.Fragment;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import butterknife.ButterKnife;
+import de.hdodenhof.circleimageview.CircleImageView;
 
 import com.crazyclimbersteam.horeca.R;
+import com.crazyclimbersteam.horeca.fragment.welcome.RegistrationFragment;
 import com.crazyclimbersteam.horeca.menu.views.MenuItemsContainer;
+
+import butterknife.InjectView;
 
 
 /**
@@ -17,14 +28,47 @@ import com.crazyclimbersteam.horeca.menu.views.MenuItemsContainer;
 public class NavigationMenuFragment extends Fragment {
     private MenuItemsContainer mMenuItemContainer;
 
+    @InjectView(R.id.user_avatar)
+    ImageView mUserAvatar;
+    @InjectView(R.id.user_name)
+    TextView mUserName;
+    @InjectView(R.id.user_email)
+    TextView mUserEmail;
+
+    SharedPreferences mRegistrationPrefs;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mRegistrationPrefs = getActivity().getSharedPreferences(RegistrationFragment.REGISTRATION_PREFS, Context.MODE_PRIVATE);
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.drawer_layout, null);
+        ButterKnife.inject(this, rootView);
+        mUserName.setText(mRegistrationPrefs.getString(RegistrationFragment.USER_NAME, ""));
+        mUserEmail.setText(mRegistrationPrefs.getString(RegistrationFragment.USER_EMAIL, ""));
+
+        //FIXME: FIND BETTER APPROACH
+        String avatarUri = mRegistrationPrefs.getString(RegistrationFragment.USER_AVATAR_URI, "");
+        if (avatarUri.length() > 0){
+            mUserAvatar.setImageURI(Uri.parse(avatarUri));
+        }else {
+            mUserAvatar.setImageResource(R.drawable.avatar_icon);
+        }
+
         mMenuItemContainer = (MenuItemsContainer) rootView.findViewById(R.id.menu_item_container);
         return rootView;
     }
 
     public void setItemClickListener(MenuItemClickListener itemClickListener) {
         mMenuItemContainer.setMenuItemClickListener(itemClickListener);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.reset(this);
     }
 }
